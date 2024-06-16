d94a4ac6fc101138aa9a586e5c2427ea4811e758
Clinton Sorrel <clintonsorrel@gmail.com>
2024-06-16 16:26:14 -0400

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,8 +1,17 @@
 
-user_prompt = "Enter a todo:"
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
-print("Todo List:")
-todos = [todo1, todo2, todo3]
-print(todos)
+user_prompt = "Type add or show"
+
+todos = []
+
+while True:
+    user_action = input("Type add, show, or exit: ")
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            print(todos)
+        case 'exit':
+            break
+        
+print('Bye!')
